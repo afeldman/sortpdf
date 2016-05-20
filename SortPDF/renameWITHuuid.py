db7078882903a3cb577fea483450aeb8b7a92258
emea\feldmaa <anton.feldmann@daimler.com>
2016-05-20 16:39:33 +0200

    rename with uuid for uniqunes

---
+++ SortPDF/renameWITHuuid.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import os
+import time
+import shutil
+import sys
+import argparse
+import uuid
+import fnmatch
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--dir", help="Source Path")
+
+args = parser.parse_args()
+
+tilefiles = []
+
+for root, dirs, files in os.walk(args.dir):
+     for name in fnmatch.filter(files, "*title*"):
+        tilefiles.append(os.path.join(root, name))
+
+if len(tilefiles) == 0:
+    sys.stdout("Error no title file!")
+
+for titlefile_path in tilefiles:
+
+    unsortedfile = os.path.join(os.path.dirname(titlefile_path),str(uuid.uuid1())+".pdf")
+  
+    try:
+        shutil.move(titlefile_path, unsortedfile)
+    except:
+        print("can not move file %s -> %s" % (titlefile_path, unsortedfile))
